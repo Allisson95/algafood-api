f35ec54e8527ca03f0f1789c7ce8a9a4d9ce3532
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:30:26 -0300

    Adicionando um objeto no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
@@ -19,4 +20,8 @@ public class CadastroCozinha {
 		return manager.createQuery("FROM Cozinha", Cozinha.class).getResultList();
 	}
 
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+	}
 }
