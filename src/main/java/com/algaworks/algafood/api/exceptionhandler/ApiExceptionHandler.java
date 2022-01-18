7283db54883427a8963e9abfae703ea1dc2ad681
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:31:19 -0300

    Desafio: customizando mensagens de validação

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +34,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private static final String MSG_ERRO_GENERICA_PARA_USUARIO = "Ocorreu um erro inesperado no sistema. Tente novamente e se o problema persistir, entre em contato com o administrador do sistema";
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@ExceptionHandler(EntidadeEmUsoException.class)
 	public ResponseEntity<?> handleEntidadeEmUso(EntidadeEmUsoException ex, WebRequest request) {
 		HttpStatus status = HttpStatus.CONFLICT;
@@ -118,10 +124,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		BindingResult bindingResult = ex.getBindingResult();
 		
 		List<Field> problemFields = bindingResult.getFieldErrors().stream()
-				.map(fieldError -> Problem.Field.builder()
+				.map(fieldError -> {
+					String message = this.messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+					
+					return Problem.Field.builder()
 						.name(fieldError.getField())
-						.userMessage(fieldError.getDefaultMessage())
-						.build())
+						.userMessage(message)
+						.build();
+				})
 				.collect(Collectors.toList());
 
 		Problem problem = createProblemBuilder(status, problemType, detail)
