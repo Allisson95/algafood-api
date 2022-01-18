a69768b650ee638ce200047ae56407b96c2bac3c
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:30:45 -0300

    Modelando e implementando a inclusão de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -19,6 +23,9 @@ public class RestauranteController {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+
+	@Autowired
+	private CadastroRestauranteService cadastroRestaurante;
 
 	@GetMapping
 	public List<Restaurante> listar() {
@@ -36,4 +43,14 @@ public class RestauranteController {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
+	@PostMapping
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+		try {
+			Restaurante restauranteSalvo = this.cadastroRestaurante.salvar(restaurante);
+
+			return ResponseEntity.status(HttpStatus.CREATED).body(restauranteSalvo);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
 }
