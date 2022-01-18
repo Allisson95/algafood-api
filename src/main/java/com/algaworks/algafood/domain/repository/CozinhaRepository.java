ae8d17efccac7222082a36d0bab819dc7e945155
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:30:30 -0300

    Repositório de restaurantes

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -7,8 +7,11 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository {
 
 	List<Cozinha> todas();
+
 	Cozinha porId(Long id);
+
 	Cozinha adicionar(Cozinha cozinha);
+
 	void remover(Cozinha cozinha);
-	
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO cozinha (nome) VALUES ('Indiana');
+
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Thai Gourmet', 10);
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Thai Delivery', 9.50);
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Tuk Tuk Comida Indiana', 15);
