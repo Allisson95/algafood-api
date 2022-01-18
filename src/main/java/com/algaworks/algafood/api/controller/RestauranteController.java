081177e84cde5f8ecb5f9eeed71cf14787370505
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:30:45 -0300

    Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -2,12 +2,14 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +51,28 @@ public class RestauranteController {
 			Restaurante restauranteSalvo = this.cadastroRestaurante.salvar(restaurante);
 
 			return ResponseEntity.status(HttpStatus.CREATED).body(restauranteSalvo);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(
+			@PathVariable("restauranteId") Long restauranteId,
+			@RequestBody Restaurante restaurante
+	) {
+		try {
+			Restaurante restauranteSalvo = this.restauranteRepository.buscar(restauranteId);
+
+			if (restauranteSalvo == null) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+
+			BeanUtils.copyProperties(restaurante, restauranteSalvo, "id");
+
+			restauranteSalvo = this.cadastroRestaurante.salvar(restauranteSalvo);
+
+			return ResponseEntity.ok(restauranteSalvo);
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		}
