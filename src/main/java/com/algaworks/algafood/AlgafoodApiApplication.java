fb98d92908f5044e49604a8708f1286b080e848b
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 02:06:20 -0300

    Corrigindo erro com uso do custom jpa repository

--- src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -2,10 +2,14 @@ package com.algaworks.algafood;
 
 import java.util.TimeZone;
 
+import com.algaworks.algafood.infrastructure.repository.CustomJpaRepositoryImpl;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
