250c0cec0d923d5924fdc6712c0c99e3b6de61fd
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:30:32 -0300

    Mapeando entidades

---
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "forma_pagamento")
+public class FormaPagamento {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String descricao;
+}
