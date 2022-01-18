d232423b9d33c1ddfb9a15d01eacb50847529396
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:30:53 -0300

    Inclusão da anotação @Repository, não comitada anteriormente, nos repositórios refatorados de cidade, estado e restaurante

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
+@Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> { }
