31452e936190b33b7b386da8a7825c6fa64a36a1
Allisson Nunes <allissonabn@gmail.com>
2022-01-18 00:30:49 -0300

    Atualização parcial de recurso com verbo http PATCH

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +22,7 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -76,5 +81,47 @@ public class RestauranteController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		}
+	}
+	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(
+			@PathVariable("restauranteId") Long restauranteId,
+			@RequestBody Map<String, Object> dados
+	) {
+		Restaurante restauranteSalvo = this.restauranteRepository.buscar(restauranteId);
+
+		if (restauranteSalvo == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+
+		restauranteSalvo = this.merge(dados, restauranteSalvo, Restaurante.class);
+
+		return this.atualizar(restauranteId, restauranteSalvo);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private <T> T merge(Map<String, Object> data, T target, Class<T> clazz) {
+		ObjectMapper mapper = new ObjectMapper();
+		T convertedValue = mapper.convertValue(data, clazz);
+
+		data.forEach((propName, propValue) -> {
+			Field field = ReflectionUtils.findField(clazz, propName);
+			field.setAccessible(true);
+
+			Object newValue;
+			if (propValue instanceof Map) {
+				newValue = merge(
+						(Map<String, Object>) propValue,
+						(T) ReflectionUtils.getField(field, target),
+						(Class<T>) field.getType()
+					);
+			} else {
+				newValue = ReflectionUtils.getField(field, convertedValue);
+			}
+
+			ReflectionUtils.setField(field, target, newValue);
+		});
+
+		return target;
 	}
 }
