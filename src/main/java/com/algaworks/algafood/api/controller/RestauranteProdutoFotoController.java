49682f543993cbc18569ffff1d20d67c2bc70fbe
Allisson Nunes <allissonabn@gmail.com>
2022-01-30 01:40:29 -0300

    melhoria no tratamento de erro da negociação de conteúdos

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -16,13 +17,16 @@ import com.algaworks.algafood.domain.service.FileStorageService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,13 +54,26 @@ public class RestauranteProdutoFotoController {
         return fotoProdutoModelAssembler.toModel(fotoSalva);
     }
 
-    @GetMapping(produces = { MediaType.IMAGE_JPEG_VALUE })
-    public ResponseEntity<InputStreamResource> buscarImagem(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+    @GetMapping(produces = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
+    public ResponseEntity<InputStreamResource> buscarImagem(
+            @PathVariable Long restauranteId,
+            @PathVariable Long produtoId,
+            @RequestHeader(name = HttpHeaders.ACCEPT) String acceptHeader
+    ) throws HttpMediaTypeNotAcceptableException {
         FotoProduto fotoSalva = catalogoFotoProduto.buscar(restauranteId, produtoId);
         InputStream arquivoFoto = fileStorage.get(fotoSalva.getNome());
 
+        MediaType mediaTypeFoto = MediaType.parseMediaType(fotoSalva.getContentType());
+        List<MediaType> mediaTypesAceitas = MediaType.parseMediaTypes(acceptHeader);
+
+        boolean naoCompativel = mediaTypesAceitas.stream()
+                .noneMatch(mediaTypeAceita -> mediaTypeAceita.isCompatibleWith(mediaTypeFoto));
+        if (naoCompativel) {
+            throw new HttpMediaTypeNotAcceptableException(List.of(mediaTypeFoto));
+        }
+
         return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_JPEG)
+                .contentType(mediaTypeFoto)
                 .body(new InputStreamResource(arquivoFoto));
     }
 
