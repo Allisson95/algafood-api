0ae0df389a2cbe6405ea970ea9b900d0c32fe100
Allisson Nunes <allissonabn@gmail.com>
2022-02-09 17:22:01 -0300

    refatorado storage config

--- src/main/java/com/algaworks/algafood/core/storage/StorageConfig.java
+++ src/main/java/com/algaworks/algafood/core/storage/StorageConfig.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.core.storage;
 
 import com.algaworks.algafood.core.storage.StorageProperties.S3;
-import com.algaworks.algafood.core.storage.StorageProperties.TypeStorage;
 import com.algaworks.algafood.domain.service.FileStorageService;
 import com.algaworks.algafood.infrastructure.service.LocalFileStorageService;
 import com.algaworks.algafood.infrastructure.service.S3FileStorageService;
@@ -20,7 +19,9 @@ public class StorageConfig {
     @Bean
     public S3Client amazonS3(StorageProperties storageProperties) {
         S3 s3Properties = storageProperties.getS3();
-        AwsCredentials credentials = AwsBasicCredentials.create(s3Properties.getAccessKey(), s3Properties.getSecretKey());
+        AwsCredentials credentials = AwsBasicCredentials.create(
+                s3Properties.getAccessKey(),
+                s3Properties.getSecretKey());
 
         return S3Client.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
@@ -30,11 +31,10 @@ public class StorageConfig {
 
     @Bean
     public FileStorageService storageService(StorageProperties storageProperties) {
-        if (storageProperties.getType() == TypeStorage.LOCAL) {
-            return new LocalFileStorageService(storageProperties);
-        } else {
-            return new S3FileStorageService(amazonS3(storageProperties), storageProperties);
-        }
+        return switch (storageProperties.getType()) {
+            case S3 -> new S3FileStorageService(amazonS3(storageProperties), storageProperties);
+            default -> new LocalFileStorageService(storageProperties);
+        };
     }
 
 }
