3f474f057485fa9d53388d889ca11e19e359fead
Allisson Nunes <allissonabn@gmail.com>
2022-02-09 22:18:09 -0300

    ajustes apontados pelo sonarlint

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,6 +4,13 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
+
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -25,13 +32,9 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.NegocioException;
-import com.fasterxml.jackson.databind.JsonMappingException.Reference;
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import com.fasterxml.jackson.databind.exc.PropertyBindingException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -41,7 +44,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	private MessageSource messageSource;
 
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> handleEntidadeEmUso(EntidadeEmUsoException ex, WebRequest request) {
+	public ResponseEntity<Object> handleEntidadeEmUso(EntidadeEmUsoException ex, WebRequest request) {
 		HttpStatus status = HttpStatus.CONFLICT;
 		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
 		String detail = ex.getMessage();
@@ -54,7 +57,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex, WebRequest request) {
+	public ResponseEntity<Object> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex, WebRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
 		String detail = ex.getMessage();
@@ -67,7 +70,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> handleNegocio(NegocioException ex, WebRequest request) {
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
 		String detail = ex.getMessage();
@@ -80,8 +83,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handleUncaught(Exception ex, WebRequest request) {
-		ex.printStackTrace();
+	public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
+		log.error("Erro inesperado", ex);
 
 		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 		String detail = MSG_ERRO_GENERICA_PARA_USUARIO;
@@ -117,12 +120,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 		Throwable rootCause = ex.getCause();
 
-		if (rootCause instanceof InvalidFormatException) {
-			return handleInvalidFormat((InvalidFormatException) rootCause, headers, status, request);
+		if (rootCause instanceof InvalidFormatException ife) {
+			return handleInvalidFormat(ife, headers, status, request);
 		}
 
-		if (rootCause instanceof PropertyBindingException) {
-			return handlePropertyBinding((PropertyBindingException) rootCause, headers, status, request);
+		if (rootCause instanceof PropertyBindingException pbe) {
+			return handlePropertyBinding(pbe, headers, status, request);
 		}
 
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
@@ -148,8 +151,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	 */
 	@Override
 	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-		if (ex instanceof MethodArgumentTypeMismatchException) {
-			return handleMethodArgumentTypeMismatch((MethodArgumentTypeMismatchException) ex, headers, status, request);
+		if (ex instanceof MethodArgumentTypeMismatchException mtme) {
+			return handleMethodArgumentTypeMismatch(mtme, headers, status, request);
 		}
 
 		return super.handleTypeMismatch(ex, headers, status, request);
@@ -188,10 +191,10 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 					.userMessage(MSG_ERRO_GENERICA_PARA_USUARIO)
 					.timestamp(OffsetDateTime.now())
 					.build();
-		} else if (body instanceof String) {
+		} else if (body instanceof String title) {
 			body = Problem.builder()
 					.status(status.value())
-					.title((String) body)
+					.title(title)
 					.userMessage(MSG_ERRO_GENERICA_PARA_USUARIO)
 					.timestamp(OffsetDateTime.now())
 					.build();
@@ -221,7 +224,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ProblemType problemType = ProblemType.PARAMETRO_INVALIDO;
 		String detail = String.format(
 				"O parâmetro de URL '%s' recebeu o valor '%s', que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
-				((MethodArgumentTypeMismatchException) ex).getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+				ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
 
 		Problem problem = createProblemBuilder(status, problemType, detail)
 				.userMessage(MSG_ERRO_GENERICA_PARA_USUARIO)
@@ -252,8 +255,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 					String message = this.messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
 					String name = objectError.getObjectName();
 
-					if (objectError instanceof FieldError) {
-						name = ((FieldError) objectError).getField();
+					if (objectError instanceof FieldError fieldError) {
+						name = fieldError.getField();
 					}
 
 					return Problem.Object.builder()
@@ -261,7 +264,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 						.userMessage(message)
 						.build();
 				})
-				.collect(Collectors.toList());
+				.toList();
 
 		Problem problem = createProblemBuilder(status, problemType, detail)
 				.userMessage(detail)
